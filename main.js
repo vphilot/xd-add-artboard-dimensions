71b3f3305e61760aa5c5cf7ce885ed91befd4dce
Vinicius Philot <vphilot@Viniciuss-MBP.ALPHA.CGLP.CA>
2019-09-30 15:16:42 -0400

    version 0.03

--- main.js
+++ main.js
@@ -1,40 +1,68 @@
 
-async function menuCommand(selection, root) {
-    // Get and show the dialog
-    const dialog = getDialog();
-    const result = await dialog.showModal();
+const { alert } = require("./lib/dialogs.js");
 
-    // Exit if the user cancels the modal
-    if (result === "reasonCanceled") {
-        return console.log("User clicked cancel or escape.");
-    } else {
-        switch (result) {
-            case "add":
-                updateArtBoardTitle(selection, root, 0);
-                break;
-            case "overwrite":
-                updateArtBoardTitle(selection, root, 1);
-                break;
+async function menuCommand(selection, root) {
+
+    let rootChildren = [];
+    root.children.forEach((childNode) => {
+        (childNode.constructor.name === "Artboard" ? rootChildren.push(true) : rootChildren.push(false));
+    });
+    
+    if (rootChildren.some ((element) => {return element === true})) {
+        //if there's at least one artboard
+        const dialog = getDialog();
+        const result = await dialog.showModal();
+
+        // Exit if the user cancels the modal
+        if (result === "reasonCanceled") {
+            return console.log("User clicked cancel or escape.");
+        } else {
+            switch (result) {
+                case "add":
+                    updateArtBoardTitle(selection, root, 0);
+                    break;
+                case "overwrite":
+                    updateArtBoardTitle(selection, root, 1);
+                    break;
+            }
         }
+    } else {
+        //if no artboards present, throw and alert and abort
+        showAlert("No Artboards found", "This plugin requires at least one artboard to be present in your document. Please create one and try again." )
+        return;
     }
 }
 
+async function showAlert(title, message) {
+    await alert(title, message);
+};
+
 function updateArtBoardTitle(selection, root, writeMode) {
-
-    root.children.forEach((childNode) => {
-        if (childNode.constructor.name === "Artboard") {
-            let w = childNode.width;
-            let h = childNode.height;
-            let n = childNode.name;
-
-            if (writeMode === 0) {
-                childNode.name = `${n}${w}x${h}`;
-            } else {
-                childNode.name = `${w}x${h}`;
+    
+    //if we have a selection, apply to the selected artboards only
+    if (selection.items.length > 0) {
+        selection.items.map(item => applyArtboardDimensionsToSelection(item, writeMode));
+    }  else {
+        //if nothing is selected, apply to the whole document
+        root.children.forEach((childNode) => {
+            if (childNode.constructor.name === "Artboard") {
+                applyArtboardDimensionsToSelection(childNode);
             }
+        });
+    }
+}
 
-        }
-    });
+function applyArtboardDimensionsToSelection(node, writeMode) {
+
+    let w = node.width;
+    let h = node.height;
+    let n = node.name;
+
+    if (writeMode === 0) {
+        node.name = `${n}${w}x${h}`;
+    } else {
+         node.name = `${w}x${h}`;
+    }
 }
 
 function getDialog() {
@@ -61,7 +89,9 @@ function createDialog() {
     </style>
     <dialog>
         <form method="dialog">
-            <h1>Please choose write mode:</h1>
+            <h1>Add Artboard Dimensions</h1>
+            <p>If you have an active selection, the plugin will only change the selected artboards. Otherwise, it will apply to the whole document.</p>
+            <p><strong>Please select a write mode:</strong></p>
             <label >
                 <input type="radio" name="writemode" id="writemode-add" value="add" checked>Add to current title<br>
                 <input type="radio" name="writemode" value="overwrite">Overwrite<br>
@@ -81,8 +111,6 @@ function createDialog() {
     const btnCancel = document.querySelector('#cancel');
     const btnOk = document.querySelector('#ok');
     const input = document.querySelectorAll('input');
-
-    // [...input].map(i => console.log(i.value));
 
     btnCancel.addEventListener("click", () => {
         dialog.close("reasonCanceled");
@@ -123,54 +151,3 @@ module.exports = {
         addSizeToArtboardTitle: menuCommand
     }
 };
-
-
-
-
-
-//------------------------
-
-
-
-
-
-// const { confirm, prompt } = require("./lib/dialogs.js");
-
-// async function addSizeToArtboardTitleHandler(selection, root) {
-//     const feedback = await confirm("Choose a mode:",
-//         "Would you like to add to your current Artboard names or overwrite them?",
-//         ["Add", "Overwrite"]);
-//     switch (feedback.which) {
-//         case 0:
-//             updateArtBoardTitle(selection, root, 0);
-//             break;
-//         case 1:
-//             updateArtBoardTitle(selection, root, 1);
-//             break;
-//     }
-// }
-
-// function updateArtBoardTitle(selection, root, writeMode) {
-
-//     root.children.forEach((childNode) => {
-//         if (childNode.constructor.name === "Artboard") {
-//             let w = childNode.width;
-//             let h = childNode.height;
-//             let n = childNode.name;
-
-//             if (writeMode === 0) {
-//                 childNode.name = `${n}${w}x${h}`;
-//             } else {
-//                 childNode.name = `${w}x${h}`;
-//             }
-
-//         }
-//     });
-// }
-
-// module.exports = {
-//     commands: {
-//         addSizeToArtboardTitle: createDialog
-//     }
-// };
-
